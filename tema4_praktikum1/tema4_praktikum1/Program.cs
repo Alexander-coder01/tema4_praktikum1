a09dc2a03834477751c6d55eaf196576c59a25dc
alexanderpostman <alexanderpostman20060319@gmail.com>
2023-11-06 09:31:11 +0300

    Create tema4_praktikum1
    add tema4_praktikum1

---
+++ tema4_praktikum1/tema4_praktikum1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema4_praktikum1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Тема№4 Практикум№1";
+            Console.CursorVisible = false;
+            Console.WindowWidth = 60;
+            Console.WindowHeight = 25;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Введите свое имя");
+            string x = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Здравствуйте {x}");
+            Console.ReadKey();
+        }
+    }
+}
